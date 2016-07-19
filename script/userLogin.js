563ef47b85e4366213de308fee00a20ac6b9df3b
hupengli <763880002@qq.com>
2016-07-19 19:02:43 +0800

    底部导航菜单栏增加

--- script/userLogin.js
+++ script/userLogin.js
@@ -1,6 +1,6 @@
 function delWord(el) {
-	var input = $api.prev(el, '.txt');
-	input.value = '';
+    var input = $api.prev(el, '.txt');
+    input.value = '';
 }
 
 //function login() {
@@ -13,53 +13,35 @@ function delWord(el) {
 //}
 
 function ensure() {
-	api.showProgress({
-		title : '正在登录...',
-		modal : false
-	});
-	var email = $api.byId('email').value;
-	var pwd = $api.byId('password').value;
+    api.showProgress({
+        title: '正在登录...',
+        modal: false
+    });
+    var name = $api.byId('username').value;
+    var pwd = $api.byId('password').value;
 
-	var loginUlr = '/user/login';
-	var bodyParam = {
-		email : email,
-		password : pwd
-	}
-	ajaxRequest(loginUlr, 'post', JSON.stringify(bodyParam), function(ret, err) {
-		if (ret) {
-			$api.setStorage('islogin', 1);
-			$api.setStorage('uid', ret.userId);
-			$api.setStorage('token', ret.id);
-			setTimeout(function() {
-				api.closeWin();
-			}, 100);
-			//			var request = '/user/userId'
-			//			var bodyP = {userId:usrId}
-			var userId = ret.userId;
-			api.alert({
-				msg : "登录成功"
-			}, function(ret, err) {
-				api.sendEvent({
-					name : 'reg_login_successEvent',
-					extra : {
-						key : true
-					}
-				});
-				// 回到首页
-				api.closeToWin({
-					name : 'root'
-				});
-			});
-		} else {
-			api.alert({
-				msg : err.msg
-			});
-		}
-		api.hideProgress();
-	})
+    var loginUlr = '/user/login';
+    var bodyParam = {
+        username: name,
+        password: pwd
+    }
+    ajaxRequest(loginUlr, 'post', JSON.stringify(bodyParam), function (ret, err) {
+        if (ret) {
+            $api.setStorage('uid', ret.userId);
+            $api.setStorage('token', ret.id);
+            setTimeout(function () {
+                api.closeWin();
+            }, 100);
+        } else {
+            api.alert({
+                msg: err.msg
+            });
+        }
+        api.hideProgress();
+    })
 }
 
-apiready = function() {
-	var header = $api.byId('header');
-	$api.fixIos7Bar(header);
+apiready = function () {
+    var header = $api.byId('header');
+    $api.fixIos7Bar(header);
 };
