b9d449c29ac90d1f21f2842156c5e00b3f6cd80c
wzt123 <1650208026@qq.com>
2016-07-26 11:45:44 +0800

    订单本地存储，数据表创建失败

--- script/localDB.js
+++ script/localDB.js
@@ -1,18 +1,61 @@
-apiready = function(){
-	var db = api.require('db');
+var db = null;
+apiready = function() {
+	db = api.require('db');
 	db.openDatabase({
-	    name: 'oderForm'
-	}, function(ret, err){        
-	    if( ret.status ){
-	        var sql = "creat table if not exist 'order' (VG_name varchar(9),VG_price float(9),VG_amount float(9),money float(9))";
-	    	db.executeSql({
-	            name:'oderForm',
-	            sql:sql
-            },function(ret,err){
-            	//coding...
-            });
-	    }else{
-	        alert( JSON.stringify( err ) );
-	    }
+		name : 'orderForm'
+	}, function(ret, err) {
+		if (ret.status) {
+			var sql = 'CREATE TABLE orderForm(VG_name varchar(255), VG_price varchar(255), FirstName varchar(255), Address varchar(255), City varchar(255))';
+			db.executeSql({
+				name : 'orderForm',
+				sql : sql
+			}, function(ret, err) {
+				if (ret) {
+					api.alert({
+						msg : "数据表创建成功或已存在"
+					});
+				} else {
+					api.alert({
+						msg : "数据表创建失败"
+					});
+				}
+			});
+		} else {
+			alert(JSON.stringify(err));
+		}
 	});
+}
+function VGdata(name, price, amount, money) {
+	db.openDatabase({
+		name : 'orderForm'
+	}, function(ret, err) {
+		if (ret) {
+			var sql = "insert into order (VG_name,VG_price,VG_amount,money) value (name,price,amount,money)"
+			db.executeSql({
+				name : 'orderForm',
+				sql : sql
+			}, function(ret, err) {
+				if (ret) {
+					api.alert({
+						msg : "本地数据插入成功"
+					}, function(ret, err) {
+						//coding...
+					});
+				} else {
+					api.alert({
+						msg : "本地数据插入失败"
+					}, function(ret, err) {
+						//coding...
+					});
+				}
+			});
+		} else {
+			api.alert({
+				msg : "本地数据库打开失败"
+			}, function(ret, err) {
+				//coding...
+			});
+		}
+	});
+
 }
