3d34eb9b2a69241290cbc95a125bf01b65e66696
wzt123 <1650208026@qq.com>
2016-07-31 16:55:31 +0800

    下啦订单完成了一半，就差显示到模版上了

--- script/orderEnsure.js
+++ script/orderEnsure.js
@@ -1,7 +1,23 @@
-function openoder() { 
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+function openoder() {
 	var username = $api.getStorage('username');
 	var userid = $api.getStorage('uid');
-	if (username == null) {
+	if (userid == null) {
 		api.alert({
 			msg : "亲，找人帮忙要先告诉别人你的电话号码哦"
 		}, function(ret, err) {
@@ -31,18 +47,21 @@ function openoder() {
 	}
 	var lon = null;
 	var lat = null;
+	var time = new Date().Format("yyyy-MM-dd hh:mm:ss");
 	var totalcash = $api.getStorage('totalcash');
 	var bodyParam = {
 		username : username,
 		money : totalcash,
 		order : order,
-		lon:lon,
-		lat:lat,
-		userid:JSON.stringify(userid),
-		classpic:classpic,
-		classname:classname
+		lon : lon,
+		lat : lat,
+		userid : JSON.stringify(userid),
+		classpic : classpic,
+		classname : classname,
+		creatTime : time,
+		rec : false
 	}
-api.showProgress({
+	api.showProgress({
 		title : '正在下单...',
 		modal : false
 	});
