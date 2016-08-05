cd37c95426365da6233240e06014ff3ccefdf43f
wzt123 <1650208026@qq.com>
2016-08-05 19:22:33 +0800

    订单带上地址完成，百度地图不知道为什么不能显示

--- script/orderEnsure.js
+++ script/orderEnsure.js
@@ -1,20 +1,30 @@
-Date.prototype.Format = function (fmt) { //author: meizz 
-    var o = {
-        "M+": this.getMonth() + 1, //月份 
-        "d+": this.getDate(), //日 
-        "h+": this.getHours(), //小时 
-        "m+": this.getMinutes(), //分 
-        "s+": this.getSeconds(), //秒 
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
-        "S": this.getMilliseconds() //毫秒 
-    };
-    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (var k in o)
-    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-    return fmt;
+Date.prototype.Format = function(fmt) {//author: meizz
+	var o = {
+		"M+" : this.getMonth() + 1, //月份
+		"d+" : this.getDate(), //日
+		"h+" : this.getHours(), //小时
+		"m+" : this.getMinutes(), //分
+		"s+" : this.getSeconds(), //秒
+		"q+" : Math.floor((this.getMonth() + 3) / 3), //季度
+		"S" : this.getMilliseconds() //毫秒
+	};
+	if (/(y+)/.test(fmt))
+		fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+	for (var k in o)
+	if (new RegExp("(" + k + ")").test(fmt))
+		fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+	return fmt;
 }
-
+var query;
+var model;
 function openoder() {
+	query = api.require('query');
+	model = api.require('model');
+
+	model.config({
+		appKey : '7810E5E4-7F54-E1E6-B07F-9ADA6FA49D46',
+		host : 'https://d.apicloud.com'
+	});
 	var username = $api.getStorage('username');
 	var userid = $api.getStorage('uid');
 	if (userid == null) {
@@ -29,12 +39,16 @@ function openoder() {
 		});
 		return;
 	}
+	api.showProgress({
+		title : '正在下单...',
+		modal : false
+	});
 	var url = '/orderform';
 	var order = {};
 	var length = $api.getStorage('jArray');
 	var classpic = $api.getStorage('classpic');
 	var classname = $api.getStorage('classname');
-	var j=0;
+	var j = 0;
 	for (var i = 0; i < length; i++) {
 		var singleorder = $api.getStorage('orderData' + '[' + i + ']');
 		singleorder = JSON.parse(singleorder);
@@ -42,46 +56,59 @@ function openoder() {
 			continue;
 		}
 		order[j] = singleorder;
-		j+=1;
-		//		api.alert({
-		//		msg:"名字："+ order[i].name+"，单价："+order[i].price+"，重量："+order[i].amount+"，金钱："+order[i].money
-		//      });
+		j += 1;
+		
 	}
-	var lon = $api.getStorage('lon');
-	var lat = $api.getStorage('lat');
-	var time = new Date().Format("yyyy-MM-dd hh:mm:ss");
-	var totalcash = $api.getStorage('totalcash');
-	var bodyParam = {
-		username : username,
-		money : totalcash,
-		order : order,
-		lon : lon,
-		lat : lat,
-		userid : JSON.stringify(userid),
-		classpic : classpic,
-		classname : classname,
-		creatTime : time,
-		rec : false
-	}
-	api.showProgress({
-		title : '正在下单...',
-		modal : false
-	});
-	ajaxRequest(url, 'post', JSON.stringify(bodyParam), function(ret, err) {
-		if (ret) {
-			api.alert({
-				msg : '下单成功！'
-			}, function() {
-				api.closeToWin({
-					name : 'root'
-				});
-			});
 
-		} else {
-			api.alert({
-				msg : err.msg
+	//获取地址
+	var lon;
+	var lat;
+	var orderid = $api.getStorage('orderid');
+	var getUserById = '/userAddress/' + orderid;
+	var bodyParam = {
+		id : orderid
+	}
+	ajaxRequest(getUserById, 'get', JSON.stringify(bodyParam), function(ret, err) {
+		if (ret) {
+			lon = ret.lon;
+			lat = ret.lat;
+			var time = new Date().Format("yyyy-MM-dd hh:mm:ss");
+			var totalcash = $api.getStorage('totalcash');
+			
+			var bodyParam = {
+				username : username,
+				money : totalcash,
+				order : order,
+				lon : lon,
+				lat : lat,
+				userid : JSON.stringify(userid),
+				classpic : classpic,
+				classname : classname,
+				creatTime : time,
+				rec : false
+			}
+
+			ajaxRequest(url, 'post', JSON.stringify(bodyParam), function(ret, err) {
+				if (ret) {
+					api.alert({
+						msg : '下单成功！'
+					}, function() {
+						api.closeToWin({
+							name : 'root'
+						});
+					});
+
+				} else {
+					api.alert({
+						msg : err.msg
+					});
+				}
+				api.hideProgress();
 			});
+		} else {
+			api.toast({
+				msg : JSON.stringify(err)
+			})
 		}
-		api.hideProgress();
-	});
+	})
 }
