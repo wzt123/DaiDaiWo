c98376fe522f3fa407b494bb2c32798eb199b5e4
wzt123 <1650208026@qq.com>
2016-09-06 13:11:24 +0800

    注册上增加了手机号，以便在送达的时候联系

--- script/userRegister.js
+++ script/userRegister.js
@@ -23,40 +23,56 @@ function delWord(el) {
 	input.value = '';
 }
 
-//if ($api.byId('username').value.length == 0 ) {
-//		document.getElementById('username').focus();
-//		api.toast({
-//			msg : '用户名不能为空'
-//		});
-//		return;
-//	} else if ($api.byId('password').value.length == 0) {
-//		document.getElementById('password').focus();
-//		api.toast({
-//			msg : '密码不能为空！'
-//		});
-//		return;
-//	} else if ($api.byId('email').value == null) {
-//
-//		document.getElementById('email').focus();
-//		api.toast({
-//			msg : '邮箱不能为空！'
-//		});
-//		return;
-//	}
-//	else
-//		var t = 1;
-
 function ensure() {
 	var email = $api.byId('email').value;
+	var tel = $api.byId('tel').value;
 	var uname = $api.byId('userName').value;
 	var pwd = $api.byId('userPwd').value;
 	var pwd2 = $api.byId('userPwd2').value;
-	if (email.length == 0||email.length!=11) {
+	if (email != "") {
+		var reg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+		var isok = reg.test(email);
+		if (!isok) {
+			api.alert({
+				msg : "邮箱格式不正确，请重新输入！"
+			}, function(ret, err) {
+				//coding...
+			});
+			document.getElementById("email").focus();
+			return;
+		}
+	} else if (email == null) {
 		api.alert({
-			msg : "电话格式不对"
+			msg : "邮箱不能为空！"
+		}, function(ret, err) {
+			//coding...
 		});
+		document.getElementById("email").focus();
 		return;
-	} else if (pwd.length == 0||pwd2.length == 0) {
+
+	} else if (tel != null) {
+		var reg = /^1[3|5|7|8][0-9]\d{4,8}$/;
+		var isok = reg.test(tel);
+		if (!isok) {
+			api.alert({
+				msg : "手机号格式不正确，请重新输入！"
+			}, function(ret, err) {
+				//coding...
+			});
+			document.getElementById("tel").focus();
+			return;
+		}
+
+	} else if (tel == null) {
+		api.alert({
+			msg : "手机号不能为空！"
+		}, function(ret, err) {
+			//coding...
+		});
+		document.getElementById("tel").focus();
+		return;
+
+	} else if (pwd.length == 0 || pwd2.length == 0) {
 		api.alert({
 			msg : "密码不能为空"
 		});
@@ -66,14 +82,12 @@ function ensure() {
 			msg : '两次密码不一致'
 		});
 		return;
-	}
-	else if (pwd.length <= 9) {
+	} else if (pwd.length <= 9 || pwd2.length <= 9) {
 		api.alert({
 			msg : "密码长度不能小于9位"
 		});
 		return;
-	}
-	  else if (uname.length == 0) {
+	} else if (uname.length == 0) {
 		api.alert({
 			msg : "用户名不能为空"
 		});
@@ -87,7 +101,8 @@ function ensure() {
 	var bodyParam = {
 		username : uname,
 		password : pwd2,
-		email : email
+		email : email,
+		tel : tel
 	}
 	ajaxRequest(registerUrl, 'post', JSON.stringify(bodyParam), function(ret, err) {
 		if (ret) {
